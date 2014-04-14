aaf26d21df047ca0f1f61678ca090f1210b23279
Louise <louiselai88@gmail.com>
2014-04-14 18:51:10 +0100

    Warning: this file makes your computer a lethal purring machine.

---
+++ Purr.rb
@@ -0,0 +1,34 @@
+# to run this, type in terminal:
+# ruby Purr.rb 
+
+def purr_application
+	
+
+	touch = gets.chomp
+
+	if touch.include? ']'
+		system( "say 'purrrrr'" )
+
+	else
+		print "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n" 
+		puts "\nI want to be stroked!! Silly human."
+		puts "\nInclude \"]\" in your stroke."
+		puts "\n...and end it with [ENTER]"
+		print "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" 
+
+	end
+	purr_application
+
+end
+purr_application
+
+# Here are some voices. Place them between ("say) and ('purrrrr'") in line 8.
+#
+# -v whisper
+# -v deranged
+# -v albert
+# -v pipe
+# (there are heaps more, ask Enrique. Apparently you can make it purr in a German voice.)
+#
+# e.g. to change to 'whisper' voice, change line 8 to:
+# system( "say -v whisper 'purrrrr'" )
